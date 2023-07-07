3916bc06ea2998cefaa633a6c015b94faccf7994
ermaneesh0077 <ermaneesh0077@gmail.com>
2023-07-08 01:43:13 +0530

    Schidular code

---
+++ FaculityReal/ConsoleApp2/VAFacilitiesCons/FileHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class FileHelper
+    {
+        public static void WriteFileAsync(string dir, string file, string content)
+        {
+            Console.WriteLine("Async Write File has started");
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(dir, file)))
+            {
+                  outputFile.Write(content);
+            }
+            Console.WriteLine("Async Write File has completed");
+            
+        }
+    }
+}
---
+++ FaculityReal/ConsoleApp2/VAFacilitiesCons/Program.cs
@@ -0,0 +1,32 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string filePath = ConfigurationManager.ConnectionStrings["FileWritePath"].ConnectionString;
+            
+
+            string vaUrl = ConfigurationManager.ConnectionStrings["VaUrl"].ConnectionString;
+            string apiKey = ConfigurationManager.ConnectionStrings["ApiKey"].ConnectionString;
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{vaUrl}");
+            request.Headers.Add("apikey", $"{apiKey}");
+            Task<HttpResponseMessage> task = Task.Run<HttpResponseMessage>(async () => await client.SendAsync(request));             
+            var jsonResponse = task.Result.Content.ReadAsStringAsync().Result;
+            FileHelper.WriteFileAsync(filePath, "Facilities.txt", jsonResponse);
+
+        }
+
+    }
+}
